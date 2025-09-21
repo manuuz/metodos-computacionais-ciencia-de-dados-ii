8c021019529ada42a46c713de9f958877ab5522e
mari fleming <marianaflemingbh@gmail.com>
2025-09-21 00:03:28 -0300

    faltando só a 2.7

--- Lista2/2.4 - ic-distribuicoes.R
+++ Lista2/2.4 - ic-distribuicoes.R
@@ -1,90 +1,55 @@
-# Definindo parâmetros globais
-M <- 20000          # Número de réplicas Monte Carlo. (Grande para erro padrão baixo)
-alfa <- 0.05       # Nível de significância
-conf_nominal <- 1 - alfa # Nível de confiança nominal (0.95)
-n_valores <- c(20, 30, 100) # Tamanhos amostrais a serem testados
-
-# Vetor para armazenar os resultados
+set.seed(2023032088)
+m <- 20000
+alfa <- 0.05
+confianca_nominal <- 1 - alfa
+n <- c(20, 30, 100)
 resultados <- list()
 i <- 1
 
-# Função auxiliar para calcular a cobertura e o erro padrão do MC
-calcular_resultados <- function(coberturas, M, nome_dist, n) {
-  Y_hat <- mean(coberturas)
-  SE_hat <- sqrt(Y_hat * (1 - Y_hat) / M)
+estimar <- function(coberturas, m, dist, n) {
+  y_chapeu <- mean(coberturas)
+  ep_estimado <- sqrt(y_chapeu * (1 - y_chapeu) / m)
   
   return(data.frame(
-    Distribuicao = nome_dist,
+    Distribuicao = dist,
     n = n,
-    Cobertura_Empirica = Y_hat,
-    Erro_Padrao_MC = SE_hat
-  ))
+    Cobertura_Empirica = y_chapeu,
+    Erro_Padrao = ep_estimado)
+    )
 }
 
-# ------------------------------------------------------------------------------
-# a) Distribuição Qui-quadrado com 2 graus de liberdade (Chi^2_2)
-# Média populacional verdadeira (mu) = df = 2.
-# ------------------------------------------------------------------------------
-
+# a
 mu_chisq <- 2
 
-for (n in n_valores) {
-  # Simulação Monte Carlo
-  # Para cada réplica, geramos a amostra, calculamos o IC t-Student de 95%,
-  # e verificamos se a verdadeira média (mu_chisq = 2) está dentro.
-  
-  coberturas_chisq <- replicate(M, expr = {
-    # Geração de amostra da Chi^2_2 (função rchisq)
+for (n in n) {
+  coberturas_chisq <- replicate(m, expr = {
     x <- rchisq(n, df = 2)
-    
-    # Cálculo do IC t-Student (utilizando a função t.test do R)
-    teste_t <- t.test(x, conf.level = conf_nominal)
-    CI <- teste_t$conf.int
-    
-    # Verificação da cobertura (y^j = 1 se coberto, 0 caso contrário)
-    CI[4] <= mu_chisq && mu_chisq <= CI[5]
-  })
-  
-  # Armazenando resultados
-  resultados[[i]] <- calcular_resultados(coberturas_chisq, M, 
-                                         "Chi^2_2 (mu=2)", n)
+    teste_t <- t.test(x, conf.level = confianca_nominal)
+    confianca <- teste_t$conf.int
+    confianca[1] <= mu_chisq && mu_chisq <= confianca[2]
+    }
+  )
+
+  resultados[[i]] <- estimar(coberturas_chisq, m, "Chi^2_2 (mu=2)", n)
   i <- i + 1
 }
 
-# ------------------------------------------------------------------------------
-# b) Outra distribuição assimétrica (Distribuição Gama)
-# Escolha: Gama(shape=2, rate=1). Média populacional verdadeira (mu) = 2/1 = 2.
-# ------------------------------------------------------------------------------
-
+# b
 mu_gamma <- 2 
 
-for (n in n_valores) {
-  # Simulação Monte Carlo
-  
-  coberturas_gamma <- replicate(M, expr = {
-    # Geração de amostra da Gama(shape=2, rate=1) (função rgamma)
-    x <- rgamma(n, shape = 2, rate = 1)
-    
-    # Cálculo do IC t-Student
-    teste_t <- t.test(x, conf.level = conf_nominal)
-    CI <- teste_t$conf.int
-    
-    # Verificação da cobertura
-    CI[4] <= mu_gamma && mu_gamma <= CI[5]
-  })
-  
-  # Armazenando resultados
-  resultados[[i]] <- calcular_resultados(coberturas_gamma, M, 
-                                         "Gamma(2, 1) (mu=2)", n)
+for (n in n) {
+  coberturas_gamma <- replicate(m, expr = {
+    x <- rgamma(n, shape = 2, rate = 1) # Gama(2,1)
+    teste_t <- t.test(x, conf.level = confianca_nominal)
+    confianca <- teste_t$conf.int
+    confianca[1] <= mu_gamma && mu_gamma <= confianca[2]
+    }
+  )
+  resultados[[i]] <- estimar(coberturas_gamma, m, "Gamma(2, 1) (mu=2)", n)
   i <- i + 1
 }
 
-# Combinando e exibindo a tabela final
 tabela_final <- do.call(rbind, resultados)
-
-# Arredondando para melhor visualização
 tabela_final$Cobertura_Empirica <- round(tabela_final$Cobertura_Empirica, 4)
-tabela_final$Erro_Padrao_MC <- round(tabela_final$Erro_Padrao_MC, 5)
-
-print("Resultados do Estudo Monte Carlo (M = 20000 réplicas)")
-print(tabela_final)
+tabela_final$Erro_Padrao <- round(tabela_final$Erro_Padrao, 5)
+kable(tabela_final, caption = "IC de 95% para distribuições assimétricas")
